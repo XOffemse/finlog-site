e4ca93ea784839138ddbb9289117821c9649725a
XOffense <xoffenseofficial@gmail.com>
2025-06-16 17:13:19 +0530

    Initial commit with project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      <About />
+    </div>
+  );
+}
+
+export default App;
